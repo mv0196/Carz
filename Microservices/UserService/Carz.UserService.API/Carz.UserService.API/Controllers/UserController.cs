f360b1be67eca7147a5aa5306794e3adecb695e7
Mukul Verma <mukulverma3148@gmail.com>
2022-01-17 00:24:15 +0530

    Cleared old folder structure, arranged in new one

---
+++ Microservices/UserService/Carz.UserService.API/Carz.UserService.API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carz.UserService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            logger.LogInformation("Index method called.");
+            return Ok();
+        }
+    }
+}
